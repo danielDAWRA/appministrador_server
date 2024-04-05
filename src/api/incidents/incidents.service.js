722ae959fa2f3054077206a87f9ca73e6d8f65f1
jaanmeld <meudza@inbox.lv>
2024-04-05 14:34:17 +0200

    replaced google drive for firebase

--- src/api/incidents/incidents.service.js
+++ src/api/incidents/incidents.service.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import uploadToGoogleDrive from '../../hooks/uploadToGoogleDrive.js';
+import uploadToFirebaseStorage from '../../hooks/uploadToFirebaseStorage.js';
 import * as incidentsRepository from './incidents.repository.js';
 import transporter from '../nodemailer.js';
 
@@ -40,6 +41,38 @@ async function getAll() {
 }
 
 async function create({ newIncident }) {
+  const incidentCopy = { ...newIncident };
+
+  if (incidentCopy.photos) {
+    // Upload each photo to Firebase Storage and replace it with the returned URL
+    const firebaseDownloadUrls = await Promise.all(incidentCopy.photos.map(async (photo) => {
+      const url = await uploadToFirebaseStorage(photo);
+
+      // Delete the file from the local filesystem
+      fs.unlink(photo, (err) => {
+        if (err) {
+          console.error(`Failed to delete file: ${err}`);
+        } else {
+          console.log(`File deleted: ${photo}`);
+        }
+      });
+
+      return url;
+    }));
+
+    incidentCopy.image = firebaseDownloadUrls;
+    console.log(firebaseDownloadUrls);
+  }
+  // As formData cannot send objects progress added in backend
+  incidentCopy.progressSteps = { title: 'Registro de incidencia', date: incidentCopy.date, note: 'Incidencia recibida' };
+
+  // Pass the incident data (with photo URLs instead of files) to the repository
+  const createdIncident = await incidentsRepository.create({ newIncident: incidentCopy });
+  return createdIncident;
+}
+
+
+async function createGoogleDriveFile({ newIncident }) {
   const incidentCopy = { ...newIncident };
 
   if (incidentCopy.photos) {
@@ -103,6 +136,7 @@ async function updateStatus({ body }) {
 export {
   getById,
   getAll,
+  createGoogleDriveFile,
   create,
   updateStatus,
 };
